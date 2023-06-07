87f4e6878c22ba7ad50c51f76a094e9856aade8d
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2023-06-07 16:16:16 +0300

    scan/matrix_selector: extend range like before
    https://github.com/vinted/promql-engine/commit/ad8b6070159d52569186653f2a060d2dd95c0002#diff-71b2f22455f49b77b1e67f02bb1ff142151a8497c0125345f63b48dca946bb0cR352
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- execution/scan/matrix_selector.go
+++ execution/scan/matrix_selector.go
@@ -404,7 +404,7 @@ func selectExtPoints(it *storage.BufferedSeriesIterator, mint, maxt int64, out [
 	extMint := mint - extLookbackDelta
 	selectsNativeHistograms := false
 
-	if len(out) > 0 && out[len(out)-1].T >= mint {
+	if len(out) > 0 && out[len(out)-1].T >= extMint {
 		// There is an overlap between previous and current ranges, retain common
 		// points. In most such cases:
 		//   (a) the overlap is significantly larger than the eval step; and/or
@@ -416,7 +416,7 @@ func selectExtPoints(it *storage.BufferedSeriesIterator, mint, maxt int64, out [
 		for drop = 0; drop < len(out) && out[drop].T <= mint; drop++ {
 
 		}
-		// Then, go back one sample if within lookbackDelta of mint.
+		// Then, go back one sample if within extLookbackDelta of mint.
 		if drop > 0 && out[drop-1].T >= extMint {
 			drop--
 		}
